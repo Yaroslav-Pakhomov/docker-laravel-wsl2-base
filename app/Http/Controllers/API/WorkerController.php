ef65841d6c731434ec7f0bb2817ac48341fcca8d
Yaroslav-Pahomov <pahomstyle90@mail.ru>
2023-08-16 16:39:41 +0500

    Класс Resource (выборка данных перед отправкой ответа API)

--- app/Http/Controllers/API/WorkerController.php
+++ app/Http/Controllers/API/WorkerController.php
@@ -5,19 +5,40 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\WorkerResource;
 use App\Models\Worker;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class WorkerController extends Controller
 {
-    public function index(): JsonResponse
+    /**
+     * @return AnonymousResourceCollection|JsonResponse
+     */
+    public function index(): AnonymousResourceCollection|JsonResponse|array
     {
         $workers = Worker::all();
-        // $workers = Worker::query()->limit(2)->get();
-        $data = [
-            'workers' => $workers,
-        ];
 
-        return response()->json($data);
+        // ->resolve() - избавляемся от слова 'data'
+        return WorkerResource::collection($workers);
+
+        // // $workers = Worker::query()->limit(2)->get();
+        // $data = [
+        //     'workers' => $workers,
+        // ];
+        //
+        // return response()->json($data);
+    }
+
+    /**
+     * @param Worker $worker
+     *
+     * @return WorkerResource|array
+     */
+    public function show(Worker $worker): WorkerResource|array
+    {
+        // ->resolve() - избавляемся от слова 'data'
+        return WorkerResource::make($worker)->resolve();
+        // 2-ой способ new WorkerResource($worker) - лучше использовать 1-ый
     }
 }
